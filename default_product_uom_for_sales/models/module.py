4a53150160423ab018aed1462afbdf5be78d5a00
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-08-08 04:13:57 +0300

    Update module.py

--- default_product_uom_for_sales/models/module.py
+++ default_product_uom_for_sales/models/module.py
@@ -5,7 +5,12 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    sale_uom_id = fields.Many2one("uom.uom", "Default Sale UoM",default=lambda self: self.uom_id.id)
+    @tools.ormcache()
+    def _get_default_uom_id(self):
+        return self.env.ref('uom.product_uom_unit')
+
+
+    sale_uom_id = fields.Many2one("uom.uom", "Default Sale UoM",default=_get_default_uom_id)
 
     @api.constrains('uom_id', 'sale_uom_id')
     def _check_uom(self):
@@ -13,23 +18,20 @@ class ProductTemplate(models.Model):
             raise ValidationError(_('The default Unit of Measure and the sales Unit of Measure must be in the same category.'))
 
 
-    @api.onchange('uom_id')
-    def _onchange_uom_id(self):
-        if self.uom_id:
-            self.sale_uom_id = self.uom_id.id
 
     @api.onchange('sale_uom_id')
     def _onchange_uom_id(self):
-        if self.uom_id and self.sale_uom_id and self.uom_id.category_id != self.sale_uom_id.category_id:
-            self.sale_uom_id = self.uom_id
-
+        for template in self:
+            if template.uom_id and template.sale_uom_id and template.uom_id.category_id != template.sale_uom_id.category_id:
+                template.sale_uom_id = template.uom_id
             
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.depends('display_type', 'product_id', 'product_packaging_qty')
-    def _compute_product_uom_qty(self):
-        res = super(SaleOrderLine, self)._compute_product_uom_qty()
+    
+    @api.depends('product_id')
+    def _compute_product_uom(self):
+        res = super(SaleOrderLine, self)._compute_product_uom()
         for line in self:
             if line.product_id and line.product_id.product_tmpl_id.sale_uom_id:
                 line.product_uom = line.product_id.product_tmpl_id.sale_uom_id
@@ -43,13 +45,13 @@ class AccountMoveLine(models.Model):
     def _compute_product_uom_id(self):
         res = super(AccountMoveLine, self)._compute_product_uom_id()
         for line in self:
-            if line.move_id.is_sale_document(include_receipts=True):
+            if line.move_id.is_sale_document(include_templateeipts=True):
                 if line.product_id:
                     if line.product_id.product_tmpl_id.sale_uom_id:
                         line.product_uom_id = line.product_id.product_tmpl_id.sale_uom_id
                     else:
                         line.product_uom_id = line.product_id.product_tmpl_id.uom_id
-            if line.move_id.is_purchase_document(include_receipts=True):
+            if line.move_id.is_purchase_document(include_templateeipts=True):
                 if line.product_id:
                     if line.product_id.product_tmpl_id.uom_po_id:
                         line.product_uom_id = line.product_id.product_tmpl_id.uom_po_id
