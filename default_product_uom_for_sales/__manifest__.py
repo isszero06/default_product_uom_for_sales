d7196213f97fbcc998b8e5c30b04c5b416987b15
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-12-01 06:40:34 +0200

    Update __manifest__.py

--- default_product_uom_for_sales/__manifest__.py
+++ default_product_uom_for_sales/__manifest__.py
@@ -9,7 +9,17 @@
     "sequence": 0,
     "summary": """ default Unit value of a product in sales and Purchase UoM for Bill """,
     'description': """
-        Set teh default Product Unit value of a product in sales and Purchase UoM for Bill
+        In Odoo, as a standard, there are two fields in the item data to define the units
+First field:
+“Unit of Measure” is unfortunately used as a default unit for inventory and also as a default unit for sale,
+but sometimes you may need to define an independent unit for sale that is different from the storage unit,
+such as the liter unit, but in selling you want another unit, such as a barrel, and the barrel unit is the default unit for sale,
+so with In this system, you can define “Default Sale UoM” to be an independent default unit for the item in the sales screens “Sales Orders - Sales Invoice - Credit Note.”
+If the “Default Sale UoM” unit is not specified, the default unit of sale will be “Unit of Measure”.
+Second field:
+"Purchase UoM"
+In this field, the default unit for purchase orders is defined, but it does not work when creating a manual invoice or creating a manual refund, and the default unit in this case is “Unit of Measure”.
+But with this program we corrected this error and made the default unit “Purchase UoM”.
       """,
     "depends": ["sale_management","account"],
     "data": [
